90d7ce54d36c61482eda3fad8ef6abf65df5296a
DMKA <DMKA@Test.com>
2020-04-19 18:12:17 +0400

    +app ico +URL -> String + Encoder

--- Anekdots/ViewController.swift
+++ Anekdots/ViewController.swift
@@ -8,8 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+func getRandomAnekdot2(block: (_ testAnekdot: String)->Void){
+    //http://rzhunemogu.ru/RandJSON.aspx?CType=1
+    if let urlRandomJoke = URL(string: "http:rzhunemogu.ru/RandJSON.aspx?CType=1") {
+        do{
+            let dataJSON = try String(contentsOf: urlRandomJoke, encoding: String.Encoding.windowsCP1251)
+            let start = dataJSON.index(dataJSON.startIndex, offsetBy: 12)
+            let end = dataJSON.index(dataJSON.endIndex, offsetBy: -2)
+            let range = start..<end
 
+            let mySubstring = dataJSON[range]  // play
+            
+            
+            //print(mySubstring)
+        
+            if !mySubstring.isEmpty{
+                    
+                    block(mySubstring.replacingOccurrences(of: "&quot", with: "\""))
+                    return
+                }
+            
+        }catch{
+            print(error)
+        }
+    }
+    block("")
+}
+
+//http:rzhunemogu.ru/Rand.aspx?CType=1
+
+
+
+class ViewController: UIViewController, XMLParserDelegate {
+    
+    @IBOutlet weak var labelAnekdots: UILabel!
+    var currentParsingElement = ""
+    
+    @IBAction func pushShareButtonAction(_ sender: Any) {
+        let activityController = UIActivityViewController(activityItems: [labelAnekdots.text!], applicationActivities: nil)
+        present(activityController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func pushRefreshButtonAction(_ sender: Any) {
+        
+        getRandomAnekdot2{ (jokeText) in
+            self.labelAnekdots.text = jokeText
+       }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
